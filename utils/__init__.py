82e926de6091b8961479fb0179eb4546ed0c1fdf
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-04-19 09:38:29 -0500

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,13 @@
+from utils import (map_difference,
+                    outliers
+                   )
+                   
+from utils.map_difference import map_diff
+from utils.outliers import std_outliers
+
+__all__ = (
+          'map_difference',
+           'map_diff',
+          'outliers',
+            'std_outliers'
+            )
