50ab32b77ad99222eecf03a9ed3d4e1636718f46
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-08-22 10:01:01 -0500

    update fill_gaps as working

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,18 @@
-from utils import (map_difference,
-                    outliers
+from utils import (colab_setup,
+                  fill_gaps,
+                  map_difference,
+                  outliers
                    )
                    
+from utils.colab_setup import setup_colab
+from utils.fill_gaps import fill_gaps
 from utils.map_difference import map_diff
 from utils.outliers import std_outliers
 
-__all__ = (
+__all__ = ('colab_setup',
+            'setup_colab',
+          'fill_gaps',
+           'fill_gaps',
           'map_difference',
            'map_diff',
           'outliers',
