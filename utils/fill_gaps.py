79745c34f18942013a095283e8992aa46c872349
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-08-30 13:43:34 -0500

    minor updates fill_gaps

--- utils/fill_gaps.py
+++ utils/fill_gaps.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import pathlib
 
-def fill_gaps(datapath, group_cols=['API_NUMBER', 'TABLE_NAME'], sort_col='TOP' , target_col='Stratcode', target='dg', match_type='contains', export_path=None):
+def fill_gaps(datapath, group_cols=['API_NUMBER', 'TABLE_NAME'], sort_col='TOP' , target_col='Stratcode', target='dg', match_type='contains', export=None):
     """Function to fill gaps in table, specifically for use where top and bottom of an interval are known and the entire interval between is assumed to be similar. 
 
     Parameters
@@ -17,7 +18,7 @@ def fill_gaps(datapath, group_cols=['API_NUMBER', 'TABLE_NAME'], sort_col='TOP' 
         Str or regular expression used to match the target that will be used for the top and bottom of the interval, by default 'dg'
     match_type : str, {'contains', 'fullmatch'}, optional
         What type of match to use for finding the target in target_col. If 'contains', uses pandas.Series.str.contains(), 'fullmatch' or 'match' uses pandas.Series.str.fullmatch(), by default 'contains'
-    export_path : str or pathlike object, by default=None, optional
+    export : str or pathlike object, by default=None, optional
         Export path to export filled table, by default None
 
     Returns
@@ -30,7 +31,7 @@ def fill_gaps(datapath, group_cols=['API_NUMBER', 'TABLE_NAME'], sort_col='TOP' 
     out_logsWithDG = logsWithDG.copy()
 
     wellLogsGrouped = logsWithDG.groupby(group_cols)
-    for wellDF in wellLogsGrouped:
+    for sortCols, wellDF in wellLogsGrouped:
         wellDF.sort_values(by=sort_col, inplace=True, ascending=True)
 
         if match_type == 'contains':
@@ -53,6 +54,10 @@ def fill_gaps(datapath, group_cols=['API_NUMBER', 'TABLE_NAME'], sort_col='TOP' 
         
         out_logsWithDG.loc[firstTarget:lastTarget, target_col] = target      
     
-    if export_path is not None:
-        out_logsWithDG.to_csv(export_path, index_label='ID')
+    if export is not None:
+        if export == True:
+            export = pathlib.Path(datapath).with_stem(pathlib.Path(datapath).stem+'_filled')
+            out_logsWithDG.to_csv(export, index_label='ID')
+        else:
+           out_logsWithDG.to_csv(export, index_label='ID')
     return out_logsWithDG
